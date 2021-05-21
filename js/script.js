9786cdb35029f0781564f7c626631983b41a7271
Davi27 <67101009+Davi27@users.noreply.github.com>
2021-05-21 10:46:43 +0100

    Meets Expectation
    The project to this point already meets the expectations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 /***********Get Elements***********/
 
+let form = document.querySelector('form');
+
 let nameField = document.getElementById("name");
+
+let emailField = document.getElementById("email");
 
 let selectJobField = document.getElementById("title");
 let otherJobField = document.getElementById("other-job-role");
@@ -10,9 +14,24 @@ let colorTShirt = document.getElementById("color");
 let designTShirtA = document.querySelectorAll("[data-theme='js puns']");
 let designTShirtB = document.querySelectorAll("[data-theme='heart js']");
 
+let allActivities = document.getElementById("activities");
+let activitiesInput = document.querySelectorAll('#activities [type= "checkbox"]');
+let priceActivities = document.getElementById("activities-cost");
+
+let selectPayment = document.getElementById('payment');
+let defaultChoicePayment = selectPayment.children[1];
+
+let cardPayment = document.getElementById('credit-card');
+let paypalPayment = document.getElementById('paypal');
+let bitcoinPayment = document.getElementById('bitcoin');
+
+let ccNum = document.getElementById('cc-num');
+let ccZip = document.getElementById('zip');
+let cvV = document.getElementById('cvv');
+
+
 
 /***********Name Fields***********/
-
 nameField.focus();
 
 
@@ -44,3 +63,137 @@ function hideWrongColors(add, remove) {
 }
 
 /***********Activities***********/
+let totalCost = 0;
+
+allActivities.addEventListener('change', e => {
+    (e.target.checked) ? totalCost += parseInt(e.target.dataset.cost) : totalCost -= parseInt(e.target.dataset.cost);
+    priceActivities.innerHTML = `Total: $${totalCost}`;
+})
+
+
+/***********Payment Info***********/
+defaultChoicePayment.setAttribute('selected', 'selected');
+paypalPayment.style.display = 'none';
+bitcoinPayment.style.display = 'none';
+
+selectPayment.addEventListener('change', e => {
+    if(selectPayment.value === 'paypal') {
+        paypalPayment.style.display = 'block';       
+        cardPayment.style.display = 'none';
+        bitcoinPayment.style.display = 'none';
+    }
+    else if(selectPayment.value === 'bitcoin') {
+        bitcoinPayment.style.display = 'block';
+        cardPayment.style.display = 'none';
+        paypalPayment.style.display = 'none';
+    }
+    else if(selectPayment.value === 'credit-card') {
+        cardPayment.style.display = 'block';
+        bitcoinPayment.style.display = 'none';
+        paypalPayment.style.display = 'none';
+    }
+})
+
+
+/***********Submit Form***********/
+form.addEventListener('submit', e => {
+    let countErrors = 0;
+
+    if(!validateName(nameField.value)) {
+        countErrors++;
+    } 
+    if(!validateEmail(emailField.value)) {
+        countErrors++;
+    } 
+    if(!validateActivities()) {
+        countErrors++;
+    } 
+    if(selectPayment.value === 'credit-card') {
+        if(!validateCard()) {
+            countErrors++;
+        }
+    } 
+
+    if(countErrors > 0) {
+        e.preventDefault();
+    }
+})
+
+
+function validateName(name) {
+    let space = /\s/g;
+    if(name.replace(space, '') === '') {
+        invalidInput(nameField);
+        return false;
+    } else {
+        validInput(nameField);
+        return true;
+    }
+}
+
+function validateEmail(email) {
+    if(/^\w+@\w+.com$/.test(email)) {
+        validInput(emailField);
+        return true;
+    } else {
+        invalidInput(emailField);
+        return false;
+    }
+}
+
+function validateActivities() {
+    return (totalCost > 0) ? true : false;
+}
+
+function validateCard() {
+    let countErrors = 0;
+
+    if(/^\d{13,16}$/.test(ccNum.value)) {
+        validInput(ccNum);
+    } else {
+        invalidInput(ccNum);
+        countErrors++;
+    }
+    if(/^\d{5}$/.test(ccZip.value)) {
+        validInput(ccZip);
+    } else {
+        invalidInput(ccZip);
+        countErrors++;
+    }
+    if(/^\d{3}$/.test(cvV.value)) {
+        validInput(cvV);
+    } else {
+        invalidInput(cvV);
+        countErrors++;
+    }
+    return (countErrors > 0) ? false : true;
+}
+
+
+/***********Accessibility***********/
+// Focus and Blur
+for(let i = 0; i < activitiesInput.length; i++) {
+    let actual = activitiesInput[i];
+
+    actual.addEventListener('focus', e => {
+        e.target.parentElement.classList.add('focus');
+    })
+    actual.addEventListener('blur', e => {
+        e.target.parentElement.classList.remove('focus');
+    })
+}
+
+// Display Errors
+
+function invalidInput(element) {
+    let target = element.parentElement;
+    target.classList.remove('valid');
+    target.classList.add('not-valid');
+    target.lastElementChild.style.display = 'block';
+}
+function validInput(element) {
+    let target = element.parentElement;
+    target.classList.remove('not-valid');
+    target.classList.add('valid');
+    target.lastElementChild.style.display = 'none';
+}
